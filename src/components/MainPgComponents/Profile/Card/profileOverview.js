e5811edec4f0679a6219d381a5fdeded62c736a3
ANS NEOphyte <jpbrown1023@gmail.com>
2019-01-02 00:35:41 -0500

    framework for mainPg up

--- src/components/MainPgComponents/Profile/Card/profileOverview.js
+++ src/components/MainPgComponents/Profile/Card/profileOverview.js
@@ -4,14 +4,11 @@ import './profileOverview.css'
 function ProfileOverview(){
 
     return (
-        <div className="profileCard bg-light mb-3">
+        <div className="profileCard">
             <div className="card-header">Profile Overview</div>
             <div className="card-body">
                 <h5 className="card-title">Light card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <div>
-                <p>Plotly</p>
+                <p className="card-text">Plotly</p>
             </div>
         </div>
     )
--- src/components/MainPgComponents/Profile/Card/userCollections.js
+++ src/components/MainPgComponents/Profile/Card/userCollections.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './userCollections.css'
 
 function UserCollections(){
     return (
         <div>
-            <div>
-                
+            <div className='userCollections'>
+                <h2>Your Collections</h2>
             </div>
         </div>
     )
