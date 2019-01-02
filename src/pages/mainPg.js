9f0f9952e8fd3aba5578236cbced0c5238d557e8
ANS NEOphyte <jpbrown1023@gmail.com>
2019-01-01 23:16:13 -0500

    mainPg changes

--- src/pages/mainPg.js
+++ src/pages/mainPg.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Avatar from '../Components/MainPgComponents/Profile/Avatar'
 
 function MainPg() {
   return (
     <div>
-      Main Page
+      <Avatar />
     </div>
   )
 }
