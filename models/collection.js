f6990c1b0bcc4ee640a351da6e96e0ca07e2d6aa
Diloretodg <diloretodg74122@gmail.com>
2019-01-03 12:30:01 -0500

    added collection.js

---
+++ models/collection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const collectionSchema = new Schema({
+  name: { type: String, required: true },
+  notes: String,
+  date: { type: Date, default: Date.now }
+});
+
+const Collection = mongoose.model("Collection", collectionSchema);
+
+module.exports = Collection;
