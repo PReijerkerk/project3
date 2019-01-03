75eb0cdfc01659513b999e4fd839fc65b5e53afb
PReijerkerk <39205356+PReijerkerk@users.noreply.github.com>
2019-01-03 18:38:49 -0500

    Update loginPg.js

--- src/pages/loginPg.js
+++ src/pages/loginPg.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../Components/LoginPgComponents/Card/card';
+import Card from '../components/LoginPgComponents/Card/card';
 
 // not sure how this will react with the collectionsPg.js state
 // class LoginPg extends Component {
