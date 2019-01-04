472f0ad84cf3a0715067e6c89a96c83af8e673a9
Ha1f_Deaf <jaredtspears@gmail.com>
2019-01-03 20:31:25 -0500

    Bootstrap modals were not working due to jQuery - the research I saw Bootstrap doesnt play too nice with normal JS so I went with the Pose JS to implement the modal for therendering of this modal from the button onClick is not working as planned - tried installing it in several ways but it is something logically I am currently missing to get this to render correctly on this stateless Card.js component. I will do more research on this tomorrow with my tutor but if someone fixes this issue before then great. Currently each modal about 85% functionality wise ready - need to work on CSS but that will be the easy part. Ha1f_Deaf Out

--- src/components/LoginPgComponents/Card/card.js
+++ src/components/LoginPgComponents/Card/card.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import LoginForm from '../LoginForm/loginForm'
+import CreateAcctForm from '../CreateAcctForm/createAcctForm'
 import './card.css';
 
 function Card() {
+
   return (
     <div>
       <div className="card border-success mb-3 text-center">
@@ -17,9 +20,25 @@ function Card() {
           </div>
             <div className="card-footer bg-dark border-success">
         
-        {/* buttons for login or sign up */}
-        <button type="button" className="btn btn-outline-success">Login</button>
-        <button type="button" className="btn btn-outline-secondary">SignUp</button>
+        {/* buttons for login or sign up 
+            Currently Not working in the onClick Function within btn element - it does work 
+            if you drag out <LoginForm/> and place it between the open/closing tags of btn - 
+            the issues then is that it is always rendered */}
+        <button type="button" className="btn btn-outline-success" onClick= {()=>(
+           <LoginForm/>
+          )}
+        >
+         Login
+         {/* <LoginForm/> cannot place here because it looks crazy and esentially is a form that is also a button */}
+        </button>
+        {/* <LoginForm/> cannot place here because the card is then the div of the form - this the buttons login and sign up have no use with this implementation */}
+        
+        <button type="button" className="btn btn-outline-secondary" onClick= {()=>(
+          <CreateAcctForm/>
+          )}>
+        SignUp
+        
+        </button>
    </div>
   </div>
 </div>
