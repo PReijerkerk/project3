e6ce9604031adbbed5c4001ea1156f6374b8ad97
Diloretodg <diloretodg74122@gmail.com>
2019-01-05 11:51:12 -0500

    forced

--- src/pages/mainPg.js
+++ src/pages/mainPg.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Profile from '../components/MainPgComponents/Profile/profile'
+import Profile from '../Components/MainPgComponents/Profile/profile'
 
 function MainPg() {
   return (
