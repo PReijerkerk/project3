8fee11c4efaad0f3efb21f0c99f002a44f8de6a5
PReijerkerk <39205356+PReijerkerk@users.noreply.github.com>
2019-01-05 13:13:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NavTabs from './Components/MainPgComponents/NavTabs/NavTabs';
-// import Navbar from './Components/MainPgComponents/NavBar/Navbar';
+import NavTabs from './components/MainPgComponents/NavTabs/NavTabs';
+// import Navbar from './components/MainPgComponents/NavBar/Navbar';
 import LoginPg from './pages/loginPg';
 import CollectionsPg from './pages/collectionsPg';
 import MainPg from './pages/mainPg';
 import AboutUs from './pages/aboutUs';
 import Contact from './pages/contact';
 import './App.css';
-// import Portfolio from './Components/PortfolioContainer';
+// import Portfolio from './components/PortfolioContainer';
 
 function App() {
     return (
