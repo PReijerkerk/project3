cdf957fed9e381f7cf6e0bd4fa05eeb4ef3df5e6
tblane <tblane88@gmail.com>
2019-01-05 13:17:50 -0500

    Commit for Controllers and models

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-//defining methods for the Collections collection
+//defining methods for the Item collection
 module.exports = {
     findAll: function(req, res) {
-        db.Collection
+        db.Item
             .find(req.query)
             .sort({date: -1})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     findById: function(req, res) {
-        db.Collection
-            .findById({userId: req.params.id})
+        db.Item
+            .findById(req.params.id)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-        db.Collection
+        db.Item
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     update: function(req, res) {
-        db.Collection
+        db.Item
             .findOneAndUpdate({_id: req.params.id}, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
-        db.Collection
+        db.Item
             .findById({_id: req.params.id})
             .then(dbModel => dbModel.remove())
             .then(dbModel=> res.json(dbModel))
