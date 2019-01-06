dbb9d069f39c14b6399904e1a5b8c1c3bfe7c8e1
ANS NEOphyte <jpbrown1023@gmail.com>
2019-01-05 22:58:31 -0500

    tried to add a footer. i changed the className to footerContainer, but the styling reverts back to container

--- src/pages/loginPg.js
+++ src/pages/loginPg.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from '../components/LoginPgComponents/Card/card'
+import Footer from '../components/MainPgComponents/Footer/footer'
 
   function LoginPg() {
     return ( 
       <div>
         <Card/>
+        <Footer />
       </div>
      );
   }
