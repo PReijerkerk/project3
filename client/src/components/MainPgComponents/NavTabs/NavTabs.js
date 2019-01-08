d5da0067cc6fa68bb9e385bb9ba98099d2fa9daf
Ha1f_Deaf <jaredtspears@gmail.com>
2019-01-08 18:27:54 -0500

    per request I re-did the NavTabs for a more legitimate UI experience. Now with this stateful NavTab.js (which may return back to a functional component when we puth this rendering of the loggedInRendering.js inside the ideal component - card.js (which is functional at this moment - the state and the subsequent loggedInRender() would be implemented on this card.js file and be initialized by something other than an onclick of the login, logout buttons. Whoever has the modals or forms set up for these buttons needs to essentially put the line 44 code of the NavTabs.js into the form's submission button (for login or logout) and dont forget to render it on line 49  NavTabs.js. I can do this once I understand how this google auth works. I want to make sure true auth is being rendered and not just an onclick to an element that has not functionally rendered back end data.

--- client/src/components/MainPgComponents/NavTabs/NavTabs.js
+++ client/src/components/MainPgComponents/NavTabs/NavTabs.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import LoggedInNavTabs from '../NavTabs/loggedInRendering'
 import "./navtabs.css";
 
-function NavTabs() {
+class NavTabs extends Component {
+ 
+  // created state to check to see if user has logged in
+  state = {
+    isLoggedIn: false
+  }
+
+  // upon being called the full navBar will Render again
+  //this should be set up for the login, logout buttons
+  loggedInRendering = () =>{
+    if(this.state.isLoggedIn){
+      return <LoggedInNavTabs/>
+    }
+  }
+
+  render(){
   return (
     <div className='nav nav-tabs navContainer'>
       <ul className='nav nav-tabs'>
@@ -14,46 +30,26 @@ function NavTabs() {
             <h1 id="logo">TrophyCase</h1>
           {/* </Link> */}
         </li>
+      
+        {/* for testing only, the onClick will not render here, just
+        rendering here for testing - would render in card.js  nearly the same as here but - we need it not to render on click of said button but on the submission of the authentication. This is something I didn't know how to do yet */}
         <li className='nav-item'>
           <Link
             to='/'
             className={window.location.pathname === '/' ? 'nav-link active' : 'nav-link'}
+            onClick= {()=>(
+              this.setState({isLoggedIn:true})
+            )}
           >
             Login
           </Link>
         </li>
-        <li className='nav-item'>
-          <Link to='/profile' className={window.location.pathname === '/profile' ? 'nav-link active' : 'nav-link'}>
-            Home
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link
-            to='/collectionsPg'
-            className={window.location.pathname === '/collectionsPg' ? 'nav-link active' : 'nav-link'}
-          >
-            Collections
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link
-            to='/aboutUs'
-            className={window.location.pathname === '/aboutUs' ? 'nav-link active' : 'nav-link'}
-          >
-            About Us
-          </Link>
-        </li>
-        <li className='nav-item'>
-          <Link
-            to='/contact'
-            className={window.location.pathname === '/contact' ? 'nav-link active' : 'nav-link'}
-          >
-            Contact
-          </Link>
-        </li>
       </ul>
+
+      {this.loggedInRendering()}
     </div>
   );
+}
 }
 
 export default NavTabs;
