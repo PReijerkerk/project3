105e23c11ea5f41ccd774826ac62493b819a96c1
Diloretodg <diloretodg74122@gmail.com>
2019-01-08 18:29:38 -0500

    added seedDB.js for collections

---
+++ scripts/seedDB.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const db = require("../database/models");
+
+// This file empties the Cases collection and inserts the Cases below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/trophycase"
+);
+
+const caseSeed = [
+
+  {
+    name: "Music",
+    story: "This is all my music stuff",
+    date: new Date(Date.now()),
+    items: [],
+    user: "Dantheman"
+  },
+  {
+    name: "Movies",
+    story: "This is all my movie stuff",
+    date: new Date(Date.now()),
+    items: [],
+    user: "Dantheman"
+  },
+  {
+    name: "Cards",
+    story: "This is all my cards",
+    date: new Date(Date.now()),
+    items: [],
+    user: "Dantheman"
+  },
+
+];
+
+db.Case
+  .remove({})
+  .then(() => db.Case.collection.insertMany(caseSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
