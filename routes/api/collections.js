7ff320eaafdf240ff233e10665a857fc1e62b9bf
Diloretodg <diloretodg74122@gmail.com>
2019-01-08 18:51:51 -0500

    reworked api/collections.js

--- routes/api/collections.js
+++ routes/api/collections.js
@@ -1,18 +1,17 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
-const collectionController = require("../../controllers/collectionsController");
+const collectionsController = require("../../controllers/collectionsController");
 
 // Matches with "/api/collections"
 router.route("/")
-  .get()
-  .post();
+.get(collectionsController.findAll)
+.post(collectionsController.create);
 
-// Matches with "/api/user/:id"
+// Matches with "/api/collection/:id"
 router
-  .route("/:id")
-  .get()
-  .get()
-  .put()
-  .delete();
+.route("/:id")
+.get(collectionsController.findById)
+.put(collectionsController.update)
+.delete(collectionsController.remove);
 
 module.exports = router;
