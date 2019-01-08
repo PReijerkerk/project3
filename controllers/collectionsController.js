efe3107764715429f508d97a2d32ea4916b698f3
tblane <tblane88@gmail.com>
2019-01-07 20:24:57 -0500

    Server running

--- controllers/collectionsController.js
+++ controllers/collectionsController.js
@@ -3,32 +3,32 @@ const db = require("../models");
 //defining methods for the Collections collection
 module.exports = {
     findAll: function(req, res) {
-        db.Collection
+        db.Case
             .find(req.query)
             .sort({date: -1})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     findById: function(req, res) {
-        db.Collection
+        db.Case
             .findById({userId: req.params.id})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-        db.Collection
+        db.Case
             .create(req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     update: function(req, res) {
-        db.Collection
+        db.Case
             .findOneAndUpdate({_id: req.params.id}, req.body)
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
-        db.Collection
+        db.Case
             .findById({_id: req.params.id})
             .then(dbModel => dbModel.remove())
             .then(dbModel=> res.json(dbModel))
