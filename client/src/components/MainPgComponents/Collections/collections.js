1cf4a0c9e0e79e808decb1d20c50976d38d52027
Ha1f_Deaf <jaredtspears@gmail.com>
2019-01-09 13:57:57 -0500

    added Dan's code to table - not rendering new data but old data is there from the DB

--- client/src/components/MainPgComponents/Collections/collections.js
+++ client/src/components/MainPgComponents/Collections/collections.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import './collections.css'
+// import React from 'react'
+// import './collections.css'
 
-  return (
-    <div className="gridContainer">
-        <div className="row">
-            <div className="col">col</div>
-            <div className="col">col</div>
-            <div className="col">col</div>
-            <div className="col">col</div>
-        </div>
-        <div className="row">
-            <div className="col">col</div>
-            <div className="col">col</div>
-            <div className="col">col</div>
-            <div className="col">col</div>
-        </div>
-        <div className="row">
-            <div className="col">col</div>
-            <div className="col">col</div>
-            <div className="col">col</div>
-            <div className="col">col</div>
-        </div>
-    </div>
-  )
+//   return (
+//     <div className="gridContainer">
+//         <div className="row">
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//         </div>
+//         <div className="row">
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//         </div>
+//         <div className="row">
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//             <div className="col">col</div>
+//         </div>
+//     </div>
+//   )
 
-export default Collections;
+// export default Collections;
