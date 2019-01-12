fb11d4f7e62189f19c0d7645244644e406e57807
Ha1f_Deaf <jaredtspears@gmail.com>
2019-01-12 11:11:22 -0500

    working on adding svg files (trophys) that will work with rellax js hopefully. Still working out how to install the npm package. Not sure if a component that is installed on the App.js is best (will be working this out later).

---
+++ client/src/svg _background_files/rellax.js
@@ -0,0 +1,17 @@
+// not sure where yet to render this relax page within our tree
+
+import React from 'react';
+
+
+
+// var rellax = new Rellax('.rellax');
+
+const Rellax = () => {
+  return ( 
+    <div>
+
+    </div>
+   );
+}
+ 
+export default Rellax;
