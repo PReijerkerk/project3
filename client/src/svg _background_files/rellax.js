637449e536a6eba6914ea4f9758c9ca9b2d5c619
Ha1f_Deaf <jaredtspears@gmail.com>
2019-01-12 12:29:13 -0500

    working still to see how to render rellax

--- client/src/svg _background_files/rellax.js
+++ client/src/svg _background_files/rellax.js
@@ -1,12 +1,11 @@
 // not sure where yet to render this relax page within our tree
 
 import React from 'react';
-
-
-
-// var rellax = new Rellax('.rellax');
+import Rellax from 'rellax'; //think this is how to call this
 
 const Rellax = () => {
+var rellax = new Rellax('.rellax');
+
   return ( 
     <div>
 
