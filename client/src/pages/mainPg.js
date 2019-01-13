32c9ded6199c86ea784cc52daa1cd454cc667048
Ha1f_Deaf <jaredtspears@gmail.com>
2019-01-13 14:36:16 -0500

    added in rellax js and it is functioning - it can be tinkered with still. I cannot figure out how or why the z-index is not rendering as directed. I am trying to have these trophies parallax behind the other profile component but it is not at this point - I tried doing individual z-indexs on the component's individual CSS files, next attempted to do it on the mainPg with inline styling and at this current position of this push I have the styling added to the App.css. None of these are working - if it cannot work we can always just change the opacity to 0. Please help if you have time.

--- client/src/pages/mainPg.js
+++ client/src/pages/mainPg.js
@@ -2,17 +2,16 @@ import React from 'react'
 import Profile from '../components/MainPgComponents/Profile/profile'
 import "../components/MainPgComponents/Profile/profile.css"
 import Footer from '../components/MainPgComponents/Footer/footer'
-
+import Parallax from '../svg _background_files/rellax';
+import './profile.css';
 function MainPg() {
   return (
     <div className="mainContainer">
-      <Profile />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+  
+      {/* should render parallax benind profile - not working */}
+      <Parallax className="SVG"/>
+      <Profile className= "profile" />
+     
       <br></br>
       <Footer />
     
