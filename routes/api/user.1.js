fb5db6e10468b2fbe7eb026c5e0bc29ca451074a
Diloretodg <diloretodg74122@gmail.com>
2019-01-14 16:37:56 -0500

    user,item,case API controller and utils synched

---
+++ routes/api/user.1.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const userController = require("../../controllers/userController.1");
+
+// Matches "/api/users"
+router.route("/")
+  .get(userController.getUsers)
+  .post(userController.createUser);
+
+// Matches '/api/users/:googleId
+router.route('/:googleId')
+  .get(userController.getUser)
+  .put(userController.addUserCollection);
+
+// matches "/api/:googleId/collections" -- retrieves all collections matching google ID
+router.route("/:googleId/collections")
+  .get(userController.userCollection);
+
+// matches "/api/all" -- retrieves all user objects
+router.route("/all")
+  .get(userController.getUsers);
+
+// Matches "/api/:googleId" -- Returns user information based on googleID
+router.route("/:googleId")
+  .get(userController.getUser);
+
+  //Matches "/api/create-user" -- create a new user in the db.Users
+router.route("/create-user")
+  .post(userController.createUser);
+
+  module.exports = router;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,30 +1,16 @@
 const router = require("express").Router();
-const userController = require("../../controllers/userController.1");
+const userController = require("../../controllers/userController");
 
-// Matches "/api/users"
+// Matches with "/api/users"
 router.route("/")
-  .get(userController.getUsers)
-  .post(userController.createUser);
+.get(userController.findAll)
+.post(userController.create);
 
-// Matches '/api/users/:googleId
-router.route('/:googleId')
-  .get(userController.getUser)
-  .put(userController.addUserCollection);
+// Matches with "/api/users/:id"
+router
+.route("/:id")
+.get(userController.findById)
+.put(userController.update)
+.delete(userController.remove);
 
-// matches "/api/:googleId/collections" -- retrieves all collections matching google ID
-router.route("/:googleId/collections")
-  .get(userController.userCollection);
-
-// matches "/api/all" -- retrieves all user objects
-router.route("/all")
-  .get(userController.getUsers);
-
-// Matches "/api/:googleId" -- Returns user information based on googleID
-router.route("/:googleId")
-  .get(userController.getUser);
-
-  //Matches "/api/create-user" -- create a new user in the db.Users
-router.route("/create-user")
-  .post(userController.createUser);
-
-  module.exports = router;
+module.exports = router;
