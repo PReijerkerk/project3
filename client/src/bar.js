aead0b646f4d52159e1c6ecdd85fe1c547d08204
ANS NEOphyte <jpbrown1023@gmail.com>
2019-01-15 23:19:19 -0500

    src/bar.js

--- client/src/bar.js
+++ client/src/bar.js
@@ -1,5 +1,5 @@
-import Carousel from "./components/LoginPgComponents/Carousel"
+// import Carousel from "./components/LoginPgComponents/Carousel"
 
 export default function bar() {
-    <Carousel />
+    // <Carousel />
   }
