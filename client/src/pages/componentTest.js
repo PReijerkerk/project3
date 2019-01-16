8fca578615b485777c80166755e748c66c48f075
Diloretodg <diloretodg74122@gmail.com>
2019-01-15 19:59:33 -0500

    nested map

--- client/src/pages/componentTest.js
+++ client/src/pages/componentTest.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from "../components/CollectionsPgComponents/Grid";
-import { Input, TextArea, FormBtn } from "../components/CollectionsPgComponents/Form";
+// import { Input, TextArea, FormBtn } from "../components/CollectionsPgComponents/Form";
 import {List, ListItem} from "../components/CollectionsPgComponents/List";
-import Thumbnail from "../components/CollectionsPgComponents/Thumbnail"
+import Card from "../components/CollectionsPgComponents/Card";
+// import Thumbnail from "../components/CollectionsPgComponents/Thumbnail"
 import API from "../utils/API";
 
 
@@ -11,9 +12,6 @@ class TestPg extends Component {
         userData:{},
         googleId: "Dantesting",
         tcase: '',
-        
-
-
     }
        
 
@@ -27,37 +25,32 @@ class TestPg extends Component {
        .catch(err => console.log(err));
    }
 
+
    
-
-
-    
 
     
 
     render(){
         return(
-         <Container>
-             <Row>
-                 <Col size="md-6">
-                        {this.state.userData.tcase ? (
-                            <List>
-                                {this.state.userData.tcase.map(tcase => 
-                                    <ListItem key={tcase._id}>
-                                        <h1>{tcase.name}</h1>
-                                        {tcase.items ? (
-                                            <div>
-                                            {tcase.items.map(item => <h1>{item.name}</h1>)}
-                                            </div>
-                                        ) : (<p>noting here, add items</p>)}
-                                    </ListItem>
-                                )}
-                            </List>
-                        ) : (
-                            <h1>there aint shit </h1>
-                        )}
-                 </Col>
-             </Row>
-         </Container>
+        <Container>
+            <Row>
+                <Col size="md-6">
+                    <Card>
+                        {this.state.userData.tcase ? 
+                            (
+                                <List>
+                                    
+                                </List>
+                            ) 
+                            : 
+                            (
+                                <h1>there aint shit </h1>
+                            )
+                        }
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
         )
     }
 }
